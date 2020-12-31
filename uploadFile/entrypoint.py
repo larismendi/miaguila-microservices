fb83a3f50a1ef8550544afebf494a725834f7871
Luis Arismendi <luisfelipe.arismendi@gmail.com>
2020-12-31 14:59:25 -0500

    readme and cors

--- uploadFile/entrypoint.py
+++ uploadFile/entrypoint.py
@@ -1,6 +1,10 @@
+from flask_cors import CORS
+from app import create_app
+
 import os
 
-from app import create_app
 
 settings_module = os.getenv('APP_SETTINGS_MODULE')
 app = create_app(settings_module)
+
+CORS(app)
